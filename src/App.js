354922bc0b7d95aa72c454b56f4dffea08cdf4f5
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 17:38:57 -0500

    :sparkles: FEATURE(App): adding header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="min-h-screen bg-gray-900">
+      <div className="w-full py-3 bg-gray-800 text-center">
+        <h1 className="text-3xl text-white font-bold">Contact Manager</h1>
+      </div>
+    </div>
   );
 }
 
