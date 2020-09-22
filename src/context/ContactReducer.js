59126338dfcf2d74f3b0f0572561a3da997a7f26
Angel Martinez <angel.codes139@gmail.com>
2020-09-22 10:14:30 -0500

    :bug: FIX(contacts): change the key of the local storage

--- src/context/ContactReducer.js
+++ src/context/ContactReducer.js
@@ -22,7 +22,7 @@ export default (state, action) => {
       };
     case UPDATE_CONTACT:
       localStorage.setItem(
-        'contact',
+        'contacts',
         JSON.stringify(
           state.contacts.map(contact =>
             contact.id === action.payload.id ? action.payload : contact
