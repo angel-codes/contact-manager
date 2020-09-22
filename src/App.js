5e22488a859a535dbfcf27628e9ed630d4b00891
Angel Martinez <angel.codes139@gmail.com>
2020-09-21 19:00:51 -0500

    :sparkles: FEATURE(contactcontext): adding context to the App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
+// Context
+import ContactState from './context/ContactState';
+
 // Components
 import Form from './components/Form';
 import ContactSection from './components/ContactSection';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900">
-      <div className="w-full py-3 bg-gray-800 text-center">
-        <h1 className="text-3xl text-white font-bold">Contact Manager</h1>
+    <ContactState>
+      <div className="min-h-screen bg-gray-900">
+        <div className="w-full py-3 bg-gray-800 text-center">
+          <h1 className="text-3xl text-white font-bold">Contact Manager</h1>
+        </div>
+        <div className="container mx-auto mt-10">
+          <Form />
+          <ContactSection />
+        </div>
       </div>
-      <div className="container mx-auto mt-10">
-        <Form />
-        <ContactSection />
-      </div>
-    </div>
+    </ContactState>
   );
 }
 
